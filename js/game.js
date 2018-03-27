3d1a24c317b085809c77b423adf26d19a40efb08
Unknown <jansierpina@gmail.com>
2018-03-27 12:15:53 +0200

    Changes fix

--- js/game.js
+++ js/game.js
@@ -1,24 +1,53 @@
-var game = new Phaser.Game(16*32, 600, Phaser.AUTO, document.getElementById('game'));
-game.state.add('Game',Game);
-game.state.start('Game');
+/*
+ * Author: Jerome Renaux
+ * E-mail: jerome.renaux@gmail.com
+ */
+
 var Game = {};
 
 Game.init = function(){
-  game.stage.disableVisibilityChange = true;
+    game.stage.disableVisibilityChange = true;
 };
 
-Game.preload = function(){
-  game.load.tilemap('map', 'assets/map/example_map.json', null, Phaser.Tilemap.TILED_JSON);
-  game.load.spritesheet('tileset', 'assets/map/tilesheet.png',32,32);
-  game.load.image('sprite','assets/sprites/sprite.png'); // this will be the sprite of the players
+Game.preload = function() {
+    game.load.tilemap('map', 'assets/map/example_map.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.spritesheet('tileset', 'assets/map/tilesheet.png',32,32);
+    game.load.image('sprite','assets/sprites/sprite.png');
 };
 
 Game.create = function(){
+    Game.playerMap = {};
+    var testKey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+    testKey.onDown.add(Client.sendTest, this);
     var map = game.add.tilemap('map');
     map.addTilesetImage('tilesheet', 'tileset'); // tilesheet is the key of the tileset in map's JSON file
     var layer;
     for(var i = 0; i < map.layers.length; i++) {
         layer = map.createLayer(i);
     }
-    layer.inputEnabled = true; // Allows clicking on the map
+    layer.inputEnabled = true; // Allows clicking on the map ; it's enough to do it on the last layer
+    layer.events.onInputUp.add(Game.getCoordinates, this);
+    Client.askNewPlayer();
+};
+
+Game.getCoordinates = function(layer,pointer){
+    Client.sendClick(pointer.worldX,pointer.worldY);
+};
+
+Game.addNewPlayer = function(id,x,y){
+    Game.playerMap[id] = game.add.sprite(x,y,'sprite');
+};
+
+Game.movePlayer = function(id,x,y){
+    var player = Game.playerMap[id];
+    var distance = Phaser.Math.distance(player.x,player.y,x,y);
+    var tween = game.add.tween(player);
+    var duration = distance*10;
+    tween.to({x:x,y:y}, duration);
+    tween.start();
+};
+
+Game.removePlayer = function(id){
+    Game.playerMap[id].destroy();
+    delete Game.playerMap[id];
 };
