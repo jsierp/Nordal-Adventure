ea603966f2ef656e9445041ed5142a9a6b3885d5
Unknown <jansierpina@gmail.com>
2018-03-28 09:33:50 +0200

    Moje ostatnie

--- js/game.js
+++ js/game.js
@@ -4,6 +4,9 @@
  */
 
 var Game = {};
+var weapon;
+var fireButton;
+var myId;
 
 Game.init = function(){
     game.stage.disableVisibilityChange = true;
@@ -13,10 +16,12 @@ Game.preload = function() {
     game.load.tilemap('map', 'assets/map/example_map.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.spritesheet('tileset', 'assets/map/tilesheet.png',32,32);
     game.load.image('sprite','assets/sprites/sprite.png');
+    game.load.image('bullet', 'assets/sprites/bullet.png');
 };
 
 Game.create = function(){
     Game.playerMap = {};
+    Game.sthMap = {};
     var testKey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
     testKey.onDown.add(Client.sendTest, this);
 
@@ -36,6 +41,10 @@ Game.create = function(){
     moveRight.onDown.add(Client.sendMove, this);
     moveRight.onUp.add(Client.finishMove, this);
 
+
+    fireButton = this.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
+    /////////////////////////////////////////////////////////////////////////////
+
     var map = game.add.tilemap('map');
     map.addTilesetImage('tilesheet', 'tileset'); // tilesheet is the key of the tileset in map's JSON file
     var layer;
@@ -52,17 +61,34 @@ Game.getCoordinates = function(layer,pointer){
 };
 
 Game.addNewPlayer = function(id,x,y){
-    Game.playerMap[id] = game.add.sprite(x,y,'sprite');
+    myId = id;
+    sprite = game.add.sprite(x,y,'sprite');
+    Game.playerMap[id] = sprite;
+
+    var style = { font: "bold 22px Arial", fill: "#fff"};
+    nick = game.add.text(0, 0, "Player " + (id+1), style);
+    nick.x = sprite.width/2-nick.width/2;
+    nick.y = -nick.height/2;
+
+    Game.playerMap[id].addChild(nick);
+        /////////////////////////////////////////////////////////////////////////////
+    //  Creates 30 bullets, using the 'bullet' graphic
+    weapon = game.add.weapon(30, 'bullet');
+
+    //  The bullet will be automatically killed when it leaves the world bounds
+    weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
+
+    //  The speed at which the bullet is fired
+    weapon.bulletSpeed = 600;
+
+    //  Speed-up the rate of fire, allowing them to shoot 1 bullet every 60ms
+    weapon.fireRate = 100;
+    weapon.trackSprite(sprite, 0, 0, true);
 };
 
 Game.movePlayer = function(id,x,y){
     Game.playerMap[id].x = x;
     Game.playerMap[id].y = y;
-    // var distance = Phaser.Math.distance(player.x,player.y,x,y);
-    // var tween = game.add.tween(player);
-    // var duration = distance*10;
-    // tween.to({x:x,y:y}, duration);
-    // tween.start();
 };
 
 Game.removePlayer = function(id){
@@ -79,4 +105,9 @@ Game.update = function() {
         Client.socket.emit('move', "LEFT");
     if(Client.right)
         Client.socket.emit('move', "RIGHT");
+
+    if (fireButton.isDown)
+    {
+        weapon.fire(Game.playerMap[myId], 100, 100);
+    }
 }
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var io = require('socket.io').listen(server);
 app.use('/css',express.static(__dirname + '/css'));
 app.use('/js',express.static(__dirname + '/js'));
 app.use('/assets',express.static(__dirname + '/assets'));
+app.use('/node_modules',express.static(__dirname + '/node_modules'));
 
 app.get('/',function(req,res){
     res.sendFile(__dirname+'/index.html');
