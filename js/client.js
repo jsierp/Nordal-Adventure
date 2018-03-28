2a37f5e278d6902f027a0c8258a5249cdfc203d5
Aksel Nooitgedagt <akselon@gmail.com>
2018-03-28 13:31:35 +0200

    Sprzątanie 2 - PHP Storm

--- js/client.js
+++ js/client.js
@@ -5,97 +5,111 @@
 var Client = {};
 Client.socket = io.connect();
 
-Client.sendTest = function(){
+Client.sendTest = function () {
     console.log("test sent");
     Client.socket.emit('test');
 };
 
-Client.sendMove = function(key){
-    let move;
-    switch(key.keyCode)
-    {
-      case Phaser.KeyCode.LEFT: move="left"; break;
-      case Phaser.KeyCode.RIGHT: move="right"; break;
-      case Phaser.KeyCode.UP: move="up"; break;
-      case Phaser.KeyCode.DOWN: move="down"; break;
+Client.sendMove = function (key) {
+    var move;
+    switch (key.keyCode) {
+        case Phaser.KeyCode.LEFT:
+            move = "left";
+            break;
+        case Phaser.KeyCode.RIGHT:
+            move = "right";
+            break;
+        case Phaser.KeyCode.UP:
+            move = "up";
+            break;
+        case Phaser.KeyCode.DOWN:
+            move = "down";
+            break;
 
     }
     Client.socket.emit('move', move);
 
-}
+};
 
-Client.sendAttack = function(){
+Client.sendAttack = function () {
 
     Client.socket.emit('attack');
 };
 
-Client.sendStop = function(key){
-    let move;
-    switch(key.keyCode)
-    {
-      case Phaser.KeyCode.LEFT: move="left"; break;
-      case Phaser.KeyCode.RIGHT: move="right"; break;
-      case Phaser.KeyCode.UP: move="up"; break;
-      case Phaser.KeyCode.DOWN: move="down"; break;
+Client.sendStop = function (key) {
+    var move;
+    switch (key.keyCode) {
+        case Phaser.KeyCode.LEFT:
+            move = "left";
+            break;
+        case Phaser.KeyCode.RIGHT:
+            move = "right";
+            break;
+        case Phaser.KeyCode.UP:
+            move = "up";
+            break;
+        case Phaser.KeyCode.DOWN:
+            move = "down";
+            break;
 
     }
     Client.socket.emit('stop', move);
 
-}
+};
 
-Client.askNewPlayer = function(){
+Client.askNewPlayer = function () {
     Client.socket.emit('newplayer');
 };
 
-Client.sendClick = function(x,y) {
-  Client.socket.emit('click',{x:x,y:y});
+Client.sendClick = function (x, y) {
+    Client.socket.emit('click', {x: x, y: y});
 };
 
-Client.socket.on('newplayer',function(data){
-    Game.addNewPlayer(data.id,data.x,data.y,data.health);
-    players[data.id]=data;
+Client.socket.on('newplayer', function (data) {
+    Game.addNewPlayer(data.id, data.x, data.y, data.health);
+    players[data.id] = data;
 });
 
-Client.socket.on('allplayers',function(data){
+Client.socket.on('allplayers', function (data) {
 
-    for(var i = 0; i < data.length; i++){
-        Game.addNewPlayer(data[i].id,data[i].x,data[i].y,data[i].health);
+    for (var i = 0; i < data.length; i++) {
+        Game.addNewPlayer(data[i].id, data[i].x, data[i].y, data[i].health);
         players[data[i].id] = data;
     }
 
-    Client.socket.on('move',function(data){
-        Game.movePlayer(data.id,data.x,data.y);
-        players[data.id]=data;
+    Client.socket.on('move', function (data) {
+        Game.movePlayer(data.id, data.x, data.y);
+        players[data.id] = data;
     });
 
-    Client.socket.on('remove',function(id){
+    Client.socket.on('remove', function (id) {
         Game.removePlayer(id);
     });
 
-    Client.socket.on('hit',function(id_attacker, id_receiver) {
-        if(players[id_receiver].health>0){
-          players[id_receiver].health--;
-          Game.playerMap[id_receiver].lives[players[id_receiver].health].kill();
-          if(players[id_receiver].health==0) Game.playerMap[id_receiver].axe.kill();
+    Client.socket.on('hit', function (id_attacker, id_receiver) {
+        if (players[id_receiver].health > 0) {
+            players[id_receiver].health--;
+            Game.playerMap[id_receiver].lives[players[id_receiver].health].kill();
+            if (players[id_receiver].health === 0) Game.playerMap[id_receiver].axe.kill();
         }
-    
+
         axe = Game.playerMap[id_attacker].axe;
-        if(Game.playerMap[id_attacker].direction == "right") {
-            tween = game.add.tween(axe).to( { angle: 45 }, 100, Phaser.Easing.Linear.None, true);
+        if (Game.playerMap[id_attacker].direction === "right") {
+            tween = game.add.tween(axe).to({angle: 45}, 100, Phaser.Easing.Linear.None, true);
             tween.onComplete.add(axeBackRight, this, id_attacker);
         } else {
-            tween = game.add.tween(axe).to( { angle: -45 }, 100, Phaser.Easing.Linear.None, true);
+            tween = game.add.tween(axe).to({angle: -45}, 100, Phaser.Easing.Linear.None, true);
             tween.onComplete.add(axeBackLeft, this, id_attacker);
         }
     });
 
-    Client.socket.on('try_hit',function(id_attacker) {
+    Client.socket.on('try_hit', function (id_attacker) {
         axe = Game.playerMap[id_attacker].axe;
-        if(Game.playerMap[id_attacker].direction == "right") {
-            tween = game.add.tween(axe).to( { angle: 45 }, 100, Phaser.Easing.Linear.None, true);
+        if (Game.playerMap[id_attacker].direction === "right") {
+            tween = game.add.tween(axe).to({angle: 45}, 100, Phaser.Easing.Linear.None, true);
             tween.onComplete.add(axeBackRight, this, id_attacker);
         } else {
-            tween = game.add.tween(axe).to( { angle: -45 }, 100, Phaser.Easing.Linear.None, true);
+            tween = game.add.tween(axe).to({angle: -45}, 100, Phaser.Easing.Linear.None, true);
             tween.onComplete.add(axeBackLeft, this, id_attacker);
         }
     });
@@ -103,10 +117,10 @@ Client.socket.on('allplayers',function(data){
 });
 
 function axeBackLeft() {
-    tween = game.add.tween(axe).to( { angle: 0 }, 100, Phaser.Easing.Linear.None, true);
+    tween = game.add.tween(axe).to({angle: 0}, 100, Phaser.Easing.Linear.None, true);
 }
 
 function axeBackRight() {
-    tween = game.add.tween(axe).to( { angle: 0 }, 100, Phaser.Easing.Linear.None, true);
+    tween = game.add.tween(axe).to({angle: 0}, 100, Phaser.Easing.Linear.None, true);
 }
 
--- server.js
+++ server.js
@@ -3,28 +3,28 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 
-app.use('/css',express.static(__dirname + '/css'));
-app.use('/js',express.static(__dirname + '/js'));
-app.use('/assets',express.static(__dirname + '/assets'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/assets', express.static(__dirname + '/assets'));
 
-app.get('/',function(req,res){
-    res.sendFile(__dirname+'/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
 server.lastPlayderID = 0;
 
-server.listen(process.env.PORT || 8081,function(){
-    console.log('Listening on '+server.address().port);
+server.listen(process.env.PORT || 8081, function () {
+    console.log('Listening on ' + server.address().port);
 });
 
-io.on('connection',function(socket){
+io.on('connection', function (socket) {
 
-    socket.on('newplayer',function(){
+    socket.on('newplayer', function () {
         socket.player = {
             id: server.lastPlayderID++,
             nick: null,
-            x: randomInt(100,400),
-            y: randomInt(100,400),
+            x: randomInt(100, 400),
+            y: randomInt(100, 400),
             keyDown: false,
             keyUp: false,
             keyLeft: false,
@@ -34,135 +34,153 @@ io.on('connection',function(socket){
             health: 10,
             lastAttack: false
         };
-        socket.emit('allplayers',getAllPlayers());
-        socket.broadcast.emit('newplayer',socket.player);
+        socket.emit('allplayers', getAllPlayers());
+        socket.broadcast.emit('newplayer', socket.player);
 
 
-        socket.on('disconnect',function(){
-            io.emit('remove',socket.player.id);
+        socket.on('disconnect', function () {
+            io.emit('remove', socket.player.id);
         });
 
 
-        socket.on('attack',function(){
-          attack(socket.player);
+        socket.on('attack', function () {
+            attack(socket.player);
         });
 
-        socket.on('move',function(move){
+        socket.on('move', function (move) {
 
-            switch(move)
-            {
-              case "right": socket.player.keyRight = true; break;
-              case "left": socket.player.keyLeft = true; break;
-              case "down": socket.player.keyDown = true; break;
-              case "up": socket.player.keyUp = true; break;
+            switch (move) {
+                case "right":
+                    socket.player.keyRight = true;
+                    break;
+                case "left":
+                    socket.player.keyLeft = true;
+                    break;
+                case "down":
+                    socket.player.keyDown = true;
+                    break;
+                case "up":
+                    socket.player.keyUp = true;
+                    break;
             }
 
         });
 
-        socket.on('stop',function(move){
-            switch(move)
-            {
-              case "right": socket.player.keyRight =false; break;
-              case "left": socket.player.keyLeft = false; break;
-              case "down": socket.player.keyDown = false; break;
-              case "up": socket.player.keyUp = false; break;
+        socket.on('stop', function (move) {
+            switch (move) {
+                case "right":
+                    socket.player.keyRight = false;
+                    break;
+                case "left":
+                    socket.player.keyLeft = false;
+                    break;
+                case "down":
+                    socket.player.keyDown = false;
+                    break;
+                case "up":
+                    socket.player.keyUp = false;
+                    break;
 
             }
 
         });
     });
 
-    socket.on('move', function(move){
+    socket.on('move', function (move) {
         switch (move) {
-          case "UP" : socket.player.y-=2; break;
-          case "DOWN" : socket.player.y+=2; break;
-          case "LEFT" : socket.player.x-=2; break;
-          case "RIGHT" : socket.player.x+=2; break;
+            case "UP" :
+                socket.player.y -= 2;
+                break;
+            case "DOWN" :
+                socket.player.y += 2;
+                break;
+            case "LEFT" :
+                socket.player.x -= 2;
+                break;
+            case "RIGHT" :
+                socket.player.x += 2;
+                break;
         }
 
-        io.emit('move',socket.player);
+        io.emit('move', socket.player);
     });
 });
 
-function mainLoop(){
-  Object.keys(io.sockets.connected).forEach(function(socketID){
-    var player = false;
-    if(io.sockets.connected[socketID])
-      var player = io.sockets.connected[socketID].player;
-      if(player)
-      {
-        if(player.keyLeft) {
-          player.x-=2;
-          player.direction = "left";
+function mainLoop() {
+    Object.keys(io.sockets.connected).forEach(function (socketID) {
+        var player = false;
+        if (io.sockets.connected[socketID])
+            var player = io.sockets.connected[socketID].player;
+        if (player) {
+            if (player.keyLeft) {
+                player.x -= 2;
+                player.direction = "left";
+            }
+            if (player.keyRight) {
+                player.x += 2;
+                player.direction = "right";
+            }
+            if (player.keyDown) {
+                player.y += 2;
+            }
+            if (player.keyUp) {
+                player.y -= 2;
+            }
+
+
+            io.emit('move', player);
         }
-        if(player.keyRight){
-           player.x+=2;
-           player.direction = "right";
-         }
-        if(player.keyDown) {
-          player.y+=2;
-        }
-        if(player.keyUp){
-           player.y-=2;
-         }
 
 
-        io.emit('move',player);
-      }
-
-
-  });
+    });
 
 }
 
 setInterval(mainLoop, 20);
 
-function getAllPlayers(){
+function getAllPlayers() {
     var players = [];
-    Object.keys(io.sockets.connected).forEach(function(socketID){
+    Object.keys(io.sockets.connected).forEach(function (socketID) {
         var player = io.sockets.connected[socketID].player;
-        if(player) players.push(player);
+        if (player) players.push(player);
     });
     return players;
 }
 
-function randomInt (low, high) {
+function randomInt(low, high) {
     return Math.floor(Math.random() * (high - low) + low);
 }
 
-function attack(player)
-{
-  //player.x=10;
+function attack(player) {
+    //player.x=10;
 
-  if(!player.lastAttack || Date.now()-player.lastAttack > 200) {
-    player.lastAttack = Date.now();
-    Object.keys(io.sockets.connected).forEach(function(socketID){
+    if (!player.lastAttack || Date.now() - player.lastAttack > 200) {
+        player.lastAttack = Date.now();
+        Object.keys(io.sockets.connected).forEach(function (socketID) {
 
-      var target = io.sockets.connected[socketID].player;
+            var target = io.sockets.connected[socketID].player;
 
-      if(target){
-        if(player.direction=="right"
-          && target.x>player.x+4 && target.x<player.x+50 && target.y < player.y+10
-          && target.y > player.y-10 && player.health>0)
-        {
-          console.log("trafiony z prawej");
-          target.health--;
-          io.emit('hit',player.id, target.id);
-          console.log(target);
+            if (target) {
+                if (player.direction == "right"
+                    && target.x > player.x + 4 && target.x < player.x + 50 && target.y < player.y + 10
+                    && target.y > player.y - 10 && player.health > 0) {
+                    console.log("trafiony z prawej");
+                    target.health--;
+                    io.emit('hit', player.id, target.id);
+                    console.log(target);
 
-        } else if(player.direction=="left" 
-          && target.x<player.x-4 && target.x>player.x-50 && target.y < player.y+10
-          && target.y > player.y-10 && player.health>0)
-        {
-          console.log("trafiony z lewej");
-          target.health--;
-          io.emit('hit',player.id, target.id);
-          console.log(target);
-        }
-        else {
-            io.emit('try_hit',player.id);
-        }
-      }
-  });
-  }
+                } else if (player.direction == "left"
+                    && target.x < player.x - 4 && target.x > player.x - 50 && target.y < player.y + 10
+                    && target.y > player.y - 10 && player.health > 0) {
+                    console.log("trafiony z lewej");
+                    target.health--;
+                    io.emit('hit', player.id, target.id);
+                    console.log(target);
+                }
+                else {
+                    io.emit('try_hit', player.id);
+                }
+            }
+        });
+    }
 }
